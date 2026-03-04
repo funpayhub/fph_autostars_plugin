35f2e3ff6416756ba469e48521053d7889c0031a
qvvonk <qvvonk@gmail.com>
2026-03-04 13:05:15 +0300

    ui changes

--- src/telegram/ui/builders.py
+++ src/telegram/ui/builders.py
@@ -2,32 +2,12 @@ from __future__ import annotations
 
 import html
 
-from autostars.src.types.enums import ErrorTypes, StarsOrderStatus
+from autostars.src.types.enums import StarsOrderStatus as SOS
 
 from funpayhub.lib.telegram.ui import Menu, MenuBuilder
 from funpayhub.lib.base_app.telegram.app.ui.ui_finalizers import StripAndNavigationFinalizer
 
 from .context import StarsOrderMenuContext
-
-
-_STATUSES = {
-    StarsOrderStatus.UNPROCESSED: '⚫ Не обрабатывался',
-    StarsOrderStatus.READY: '🟡 Готов к выполнению',
-    StarsOrderStatus.TRANSFERRING: '⏳ Выполняется перевод',
-    StarsOrderStatus.DONE: '🟢 Выполнен',
-    StarsOrderStatus.WAITING_FOR_USERNAME: '❓ Ожидается Telegram юзернейм',
-    StarsOrderStatus.ERROR: '❌ Произошла ошибка',
-}
-
-
-_ERRORS = {
-    ErrorTypes.WALLET_NOT_PROVIDED: 'TON кошелек не инициализирован (сид фраза не была указана в настройках в момент заказа).',
-    ErrorTypes.FRAGMENT_API_NOT_PROVIDED: 'Fragment API не инициализирован (cookie или hash не были указаны в настройках в момент заказа).',
-    ErrorTypes.UNABLE_TO_FETCH_USERNAME: 'Ошибка Fragment API: не удалось проверить Telegram юзернейм.',
-    ErrorTypes.NOT_ENOUGH_TON: 'Недостаточно TON.',
-    ErrorTypes.TRANSFER_ERROR: 'Ошибка при переводе TON. Подробности в логах.',
-    ErrorTypes.UNKNOWN: 'Неизвестная ошибка. Подробности в логах.',
-}
 
 
 class StarsOrderInfoMenuBuilder(
@@ -45,16 +25,17 @@ class StarsOrderInfoMenuBuilder(
             '🤠 <b><i>Покупатель: {buyer}</i></b>\n'
             '✨ <b><i>Кол-во: {stars_amount}</i></b>\n'
             '👤 <b><i>Telegram: @{telegram_username}</i></b>\n'
+            '🐙 <b><i>FunPay Hub ID:</i></b> <code>{hub_instance}</code>\n'
             '📍 <b><i>Статус: {status}</i></b>\n'
         ).format(
             message=html.escape(ctx.stars_order.message_obj.text),
             buyer=ctx.stars_order.order_preview.counterparty.username,
             stars_amount=ctx.stars_order.stars_amount,
             telegram_username=html.escape(ctx.stars_order.telegram_username),
-            status=_STATUSES.get(ctx.stars_order.status, ctx.stars_order.status.name),
+            status=ctx.stars_order.status.desc,
         )
-        if ctx.stars_order.status == StarsOrderStatus.ERROR and ctx.stars_order.error is not None:
-            menu.main_text += _ERRORS[ctx.stars_order.error] + '\n'
+        if ctx.stars_order.status in [SOS.ERROR, SOS.WAITING_FOR_USERNAME] and ctx.stars_order.error is not None:
+            menu.main_text += ctx.stars_order.error.desc
 
         menu.main_text += '\n'
         if ctx.stars_order.recipient_id:
@@ -71,9 +52,22 @@ class StarsOrderInfoMenuBuilder(
                 )
             )
 
-        if ctx.stars_order.status is StarsOrderStatus.DONE and ctx.stars_order.transaction_hash:
+        if ctx.stars_order.in_msg_hash:
+            menu.main_text += {
+                '#️⃣ <b><i>Hash полезной нагрузки:</i></b> <code>{hash}</code>\n'.format(
+                    hash=ctx.stars_order.in_msg_hash
+                )
+            }
+
+        if ctx.stars_order.status is SOS.DONE and ctx.stars_order.transaction_hash:
             menu.main_text += '#️⃣ <b><i>Hash транзакции:</i></b> <code>{hash}</code>\n'.format(
                 hash=ctx.stars_order.transaction_hash,
+            )
+
+            menu.header_keyboard.add_url_button(
+                button_id='open_transasction',
+                text='Открыть транзакцию',
+                url=f'https://tonviewer.com/transaction/{ctx.stars_order.transaction_hash}',
             )
 
         menu.main_text = menu.main_text.strip()
